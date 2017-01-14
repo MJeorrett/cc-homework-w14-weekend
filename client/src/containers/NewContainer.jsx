1b2769275ac71a5a67cfa10a71c4c6e265ad00c0
Matthew Jeorrett <matthewjeorrett@gmail.com>
2017-01-14 11:24:32 +0000

    adding title input to NewContainer

--- client/src/containers/NewContainer.jsx
+++ client/src/containers/NewContainer.jsx
@@ -2,10 +2,26 @@ import React from 'react'
 
 class NewContainer extends React.Component {
 
+  constructor() {
+    super()
+    this.state = {
+      title: ""
+    }
+    this.handleTitleChanged = this.handleTitleChanged.bind( this )
+  }
+
+  handleTitleChanged( ev ) {
+    var newTitle = ev.target.value
+    this.setState({
+      title: newTitle
+    })
+    console.log( "Title changed to:", newTitle )
+  }
+
   render() {
     return (
       <nav>
-        <span>New Container</span>
+        <input type="text" onChange={ this.handleTitleChanged } value={ this.props.title } />
       </nav>
     )
   }
