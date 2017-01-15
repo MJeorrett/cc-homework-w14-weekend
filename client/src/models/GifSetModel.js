73df9858dd74876183392d4b208abf15e349ff80
Matthew Jeorrett <matthewjeorrett@gmail.com>
2017-01-15 14:26:32 +0000

    adding tests for creating GifSetModel with data

--- client/src/models/GifSetModel.js
+++ client/src/models/GifSetModel.js
@@ -1,10 +1,30 @@
 "use strict"
+var GifModel = require('./GifModel')
 
 class GifSetModel {
-  constructor() {
-    this.title = ""
-    this.questions = []
+  constructor( data ) {
+    if ( !data ) data = {}
+
+    this.title = data.title || ""
+
     this.gifs = []
+    if ( data.gifs ) {
+      this.gifs = data.gifs.map( (gifData) => {
+        return new GifModel( gifData )
+      })
+    }
+  }
+
+  questions() {
+    var questions = []
+    this.gifs.forEach( (gifModel) => {
+      gifModel.questions.forEach( (question) => {
+        if ( questions.indexOf( question ) === -1 ) {
+          questions.push( question )
+        }
+      })
+    })
+    return questions
   }
 }
 
--- client/src/models/specs/GifSetModel_specs.js
+++ client/src/models/specs/GifSetModel_specs.js
@@ -7,9 +7,29 @@ var GifSetModel = require('../GifSetModel')
 describe( "GifSetModel", () => {
 
   var newGifSetModel;
+  var loadedGifSetModel;
 
   beforeEach( () => {
     newGifSetModel = new GifSetModel()
+    loadedGifSetModel = new GifSetModel({
+      title: "Banana",
+      gifs: [
+        {
+          url: "first_url",
+          questions: [
+            "First question",
+            "Second question",
+            "Third question"
+          ]
+        },
+        {
+          url: "second_url",
+          questions: [
+            "Second question"
+          ]
+        }
+      ]
+    })
   })
 
 // empty constructor tests
@@ -18,10 +38,24 @@ describe( "GifSetModel", () => {
   })
 
   it( "should start with 0 questions", () => {
-    assert.equal( 0, newGifSetModel.questions.length )
+    assert.equal( 0, newGifSetModel.questions().length )
   })
 
   it( "should start with 0 gifs", () => {
     assert.equal( 0, newGifSetModel.gifs.length )
   })
+
+// loading gifset test
+  it( "should have title passed in constructor", () => {
+    assert.equal( "Banana", loadedGifSetModel.title )
+  })
+
+  it( "should have number of gifs passed in constructor", () => {
+    assert.equal( 2, loadedGifSetModel.gifs.length )
+  })
+
+  it( "should have number of unique questions passed in constructor", () => {
+    assert.equal( 3, loadedGifSetModel.questions().length )
+  })
+
 })
