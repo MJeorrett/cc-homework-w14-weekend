8915037ea6f0c00f91e85a062cb9058796d65b01
Matthew Jeorrett <matthewjeorrett@gmail.com>
2017-01-15 17:06:36 +0000

    enabling api

--- client/src/helpers/XmlHttpHelper.jsx
+++ client/src/helpers/XmlHttpHelper.jsx
@@ -3,18 +3,18 @@ import mockData from '../MockGiphResponse'
 const XmlHttpHelper = {
 
   get( url, onloadCallback ) {
-    // var req = new XMLHttpRequest()
-    // req.open( 'GET', url )
-    // req.onload = () => {
-    //   const dataObject = JSON.parse( req.responseText )
-    //   if( req.status === 200 ) {
-    //     onloadCallback( dataObject )
-    //   } else {
-    //     console.log( "Error GET from:", url, ", failed with status: ", req.status, "/n", dataObject );
-    //   }
-    // }
-    // req.send( null )
-    onloadCallback( mockData )
+    var req = new XMLHttpRequest()
+    req.open( 'GET', url )
+    req.onload = () => {
+      const dataObject = JSON.parse( req.responseText )
+      if( req.status === 200 ) {
+        onloadCallback( dataObject )
+      } else {
+        console.log( "Error GET from:", url, ", failed with status: ", req.status, "/n", dataObject );
+      }
+    }
+    req.send( null )
+    // onloadCallback( mockData )
   },
 
   post( url, payload, onloadCallback ) {
