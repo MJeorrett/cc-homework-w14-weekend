d65754300650090d68fc638665ddd11eb123efe1
Matthew Jeorrett <matthewjeorrett@gmail.com>
2017-01-15 13:56:32 +0000

    adding tests for adding and removing questions

--- client/src/models/GifModel.js
+++ client/src/models/GifModel.js
@@ -5,6 +5,17 @@ class GifModel {
     this.url = url
     this.questions = questions || []
   }
+
+  addQuestion( question ) {
+    this.questions.push( question )
+  }
+
+  removeQuestion( question ) {
+    var questionIndex = this.questions.indexOf( question )
+    if (  questionIndex !== -1 ) {
+      this.questions.splice( questionIndex, 1 )
+    }
+  }
 }
 
 module.exports = GifModel
--- client/src/models/specs/GifModel_spec.js
+++ client/src/models/specs/GifModel_spec.js
@@ -7,9 +7,15 @@ var GifModel = require('../GifModel')
 describe( 'GifModel', () => {
 
   var testGifModel
+  var gifModelWithQuestions
 
   beforeEach( () => {
     testGifModel = new GifModel( 'http://test-url' )
+    gifModelWithQuestions = new GifModel( 'test', [
+      "Where is the world?",
+      "Where is Wally?",
+      "How do you know me?"
+    ])
   })
 
   it( "should have url passed in constructor", () => {
@@ -24,6 +30,18 @@ describe( 'GifModel', () => {
       var mockQuestions = "mock questions"
       var gifModel = new GifModel( "test", mockQuestions )
       assert.equal( mockQuestions, gifModel.questions )
+  })
+
+  it( "should add question passed to 'addQuestion' to questions array", () => {
+    var mockQuestion = "question";
+    testGifModel.addQuestion( mockQuestion )
+    assert.notEqual( -1, testGifModel.questions.indexOf( mockQuestion ) )
+  })
+
+  it( "should remove question passed to 'removeQuestion' from questions array", () => {
+    gifModelWithQuestions.removeQuestion( "How do you know me?" )
+    assert.equal( 2, gifModelWithQuestions.questions.length )
+    assert.equal( -1, gifModelWithQuestions.questions.indexOf( "How do you know me?" ))
   })
 
 })
